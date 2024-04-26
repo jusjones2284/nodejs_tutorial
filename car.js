891c0d99e9eb5b1f7b77c539c548956e57a494c7
Justin Jones <jus.jones@yahoo.com>
2024-04-26 12:38:41 -0500

    updated node.js file

---
+++ car.js
@@ -0,0 +1,22 @@
+const ford = {
+    branch: "Ford", 
+    model: "Fiesta"
+};
+
+const honda ={
+    branch: "Honda",
+    model: "Accord"
+}
+
+// module.exports = honda; 
+// exports.data = {ford, honda}; 
+
+exports.tesla = {
+    branch:  "Tesla",
+    model: "Model 3"
+}
+
+exports.lexus = {
+    branch: "Lexus",
+    model: "Rx 350"
+}
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-console.log("This is Nodjs Tutorial for beginners");
-console.log("This is the first tutorial on nodejs");
-console.log("We will cover Nodejs with command line");
-// process.exit(0)
+// console.log("This is Nodjs Tutorial for beginners");
+// console.log("This is the first tutorial on nodejs");
+// console.log("We will cover Nodejs with command line");
+// // process.exit(0)
 
-console.log("This is the first tutorial on nodejs");
-console.log("This is the first tutorial on nodejs");
-console.log("Where are you going");
-// process.exitCode = 1;
+// console.log("This is the first tutorial on nodejs");
+// console.log("This is the first tutorial on nodejs");
+// console.log("Where are you going");
+// // process.exitCode = 1;
+
+// const _ = require("lodash");
+// const arr = [1, 4, 6, 8]
+// console.log(_.first(arr));
+// console.log(_.chunk(arr));
+
+// console.log(_.filter((arr)=> arr[1] === 4));
+
+// const cowsay = require("cowsay"); 
+// console.log((cowsay.say),{
+//     text: "I am learning NPM modulel",
+//     e: "00",
+//     t: "U"
+// });
+
+const { tesla, lexus } = require("./car");
+console.log(JSON.stringify(tesla, null, 2));
+console.log(JSON.stringify(lexus, null, 1));
+console.log(lexus);
